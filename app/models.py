2b052fb83a628890418a4c519ae90247c29fbb38
Munge Ryan <mungeryan@gmail.com>
2020-07-21 12:44:54 +0300

    importing flask migrate

--- app/models.py
+++ app/models.py
@@ -1,7 +1,6 @@
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash,check_password_hash
 from . import login_manager
-from datetime import datetime
 from sqlalchemy.sql import func
 from . import db
 

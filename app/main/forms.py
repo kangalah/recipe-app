758c86597073317e6aaae962219572efda21cfaf
kangalah <nyongesajoannanjala@gmail.com>
2020-07-21 11:27:38 +0300

    create main blueprint folder

---
+++ app/main/forms.py
@@ -0,0 +1,10 @@
+# from flask_wtf import FlaskForm
+# from wtforms import StringField, SubmitField, PasswordField
+# from wtforms.validators import Required
+
+
+# class RegistrationForm(FlaskForm):
+#     name = StringField('Name', validators=[Required()])
+#     email = StringField('Email', validators=[Required()])
+#     password = PasswordField('Password', validators=[Required()])
+#     submit = SubmitField('Register')
